066af01dd5ff1628b1760a137d95dc2b32f6a3a2
rafter1515 <96643634+rafter1515@users.noreply.github.com>
2021-12-25 07:31:38 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 key='1cfa39c0-cffc-41c1-8b37-62f8575f4b29'
 app_name="accgenhertz"
-deviceid=""
+deviceid="32F15531D95B29601F3B97D2B43BD0E7F760C7D3C938340CE4D8FE1F43E5B262C780DCB0EB213E89A0"
 emaill="9uu8xdwj8q@yoggm.com"
 passwordd="78787878"
 custompwd="78787878"
-private=""
-chatlink=""
-nickname=""
+private="558ccfe7-7360-4c43-b6ab-cc85059dd4e4"
+chatlink="https://aminoapps.com/p/lp7dse"
+nickname="hertz"
