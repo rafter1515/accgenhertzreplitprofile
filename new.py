b7000823b7180b2c105603087565d31efea0f28c
rafter1515 <96643634+rafter1515@users.noreply.github.com>
2022-01-14 00:57:47 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,10 +1,10 @@
 key='1cfa39c0-cffc-41c1-8b37-62f8575f4b29'
 app_name="accgenhertz"
 deviceid="32F15531D95B29601F3B97D2B43BD0E7F760C7D3C938340CE4D8FE1F43E5B262C780DCB0EB213E89A0"
-emaill="9uu8xdwj8q@yoggm.com"
+emaill="608n0cs99e@1secmail.com"
 passwordd="78787878"
 custompwd="78787878"
 private="558ccfe7-7360-4c43-b6ab-cc85059dd4e4"
-chatlink="https://aminoapps.com/p/lp7dse"
+chatlink="http://aminoapps.com/p/g248kc"
 nickname="hertz"
 replit="https://accgenhertzezeafefg1zearhg564z1r65hg41rz5h61zreh23z1r2hrz1h.hakrmc18.repl.co"
